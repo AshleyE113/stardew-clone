4325982dd7a1ffdea8651c02e29d64a35a60a7fb
Ashley Sarah Eliassaint <ase330@nyu.edu>
2020-11-04 23:56:35 -0500

    Added two basic sprites  (char and the 'landplot'), also added three scripts, movement, a quick one that can generate a background with a sprite, and started the land plot
    WARNING: There is an issue with the Farming script. Comment it out to try the other ones out for now!

---
+++ StardewValleyClone/Assets/Farming.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Farming : MonoBehaviour
+{
+    //Is it better to have an array or an ArrayList?
+    //ArrayLists are muatable while arrays aren't
+    // List pf 5 can change, an array of 5 can't...
+    public int[] crops;
+
+    public GameObject LandPlot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int land_row = 0; land_row < 6; land_row++)
+        {
+            for (int land_col = 5; land_col > 0; land_col--)
+            {
+                crops[land_row][land_col] = LandPlot;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
