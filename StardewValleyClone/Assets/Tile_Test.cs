c915f1951138d01acf47dd5aa1af65be694cff4b
ax350 <ax350@nyu.edu>
2020-11-05 11:41:59 -0500

    Added a test file, ignore or delete later

---
+++ StardewValleyClone/Assets/Tile_Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile_Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Hey! This is a test script file only to check if I'm working on the branch or not!
+        //And whether I can merge it in main!
+        //Ignore or delete this later!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
