e9348c60e44edb6cb90b13ef2cbea0969d256951
Ashley Sarah Eliassaint <ase330@nyu.edu>
2020-11-19 13:00:35 -0500

    Merge branch 'main' into AshBranch

---
+++ StardewValleyClone/Assets/Scipts/FishingLine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingLine : MonoBehaviour
+{
+    public GameObject rodTip;          // Reference to the starting pt
+    public GameObject baitPt;          // Reference to the ending point
+    public float lineWidth;
+    private LineRenderer line;                // Line Renderer
+ 
+     // Use this for initialization
+     void Start () {
+         // Add a Line Renderer to the GameObject
+         line = this.gameObject.GetComponent<LineRenderer>();
+         // Set the width of the Line Renderer
+         line.SetWidth(lineWidth, lineWidth);
+         // Set the number of vertex fo the Line Renderer
+         line.SetVertexCount(2); //the vertex must be 2
+     }
+     
+     void Update () {
+         // Check if the GameObjects are not null
+         if (rodTip != null && baitPt != null)
+         {
+             // Update position of the two vertex of the Line Renderer
+             line.SetPosition(0, rodTip.transform.position);
+             line.SetPosition(1, baitPt.transform.position);
+         }
+     }
+ }
---
+++ StardewValleyClone/Assets/Script/TileManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileManager : MonoBehaviour
+{
+    public static TileManager tileManager;
+    public Tilemap GroundTilemap;
+    public List<TileData> tileDatas;
+    public Dictionary<TileBase, TileData> allTiles;
+
+
+    private void Awake()
+    {
+        if (tileManager == null)
+        {
+            tileManager = this;
+        }
+        else if (tileManager != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetAllTiles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GetAllTiles()
+    {
+        allTiles = new Dictionary<TileBase, TileData>();
+        foreach (var tileData in tileDatas)
+        {
+            foreach (var tile in tileData.tiles)
+            {
+                allTiles.Add(tile, tileData);
+                if (tileData.Name == "Water")
+                { 
+                    
+                }
+                Debug.Log(tile + " + " + tileData);
+                //Debug.Log(GroundTilemap.GetTile(new Vector3Int(0,0,0)));
+            }
+        }
+    }
+
+    public Vector3Int MatchTile(Vector3Int pos)
+    {
+        //Debug.Log(GroundTilemap.cellBounds);
+        //Debug.Log("A "+GroundTilemap.cellBounds.allPositionsWithin);
+        //return GroundTilemap.GetTile(GroundTilemap.WorldToCell(pos));
+
+        var tmp_tile = GroundTilemap.GetTile(GroundTilemap.WorldToCell(pos));
+        Debug.Log("Seedable " + allTiles[tmp_tile].Seedable);
+
+
+        return GroundTilemap.WorldToCell(pos);
+    }
+}
