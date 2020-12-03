c97f4e3962baef96793a931ae75ac8dda27fbe9d
Ashley Sarah Eliassaint <ase330@nyu.edu>
2020-12-03 12:14:07 -0500

    //Planting script fully complete with one plant being able to grow

--- StardewValleyClone/Assets/Script/ToPlant.cs
+++ StardewValleyClone/Assets/Script/ToPlant.cs
@@ -10,13 +10,20 @@ public class ToPlant : MonoBehaviour
     public bool OnLand;
     public bool isDragging;
     public GameObject newInstance;
+    public bool readyToHarvest;
+    public bool inHere;
+    public Sprite[] Plant1;
+    public float Start_Time = 40;
+    //private int plant_index = 0;
 
-    //For Grow fun
+    WaitForSeconds delay = new WaitForSeconds(5f);
+    
 
     ///*
     void Start()
     {
-        //newInstance = Instantiate(Seeds, new Vector3(0, 0, 0), Quaternion.identity);        
+        readyToHarvest = false;
+        inHere = false;       
     }
     //*/
 
@@ -38,13 +45,53 @@ public class ToPlant : MonoBehaviour
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             transform.Translate(mousePosition);
         }
+
+        if (OnLand == true && readyToHarvest == false && inHere == false)
+        {
+            StartCoroutine(ShowGrowth());
+        }
     } 
+
+    IEnumerator ShowGrowth()
+    {
+        /*Destroy(Plant1[plant_index++]);
+        if (plant_index < Plant1.Length)
+        {
+            Invoke("ShowGrowth", 5f);
+        }*/
+        inHere = true;
+        for (int i = 0; i < Plant1.Length; i++)
+        {
+            Debug.Log("In loop");
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Plant1[i];
+            yield return delay;
+
+            if (i == Plant1.Length-1)
+            {
+                Debug.Log("breaking");
+                readyToHarvest = true;
+                inHere = false;
+                break;
+            }
+            
+            /* if (Start_Time % 5 == 0)
+            {
+                Debug.Log("In if");
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = Plant1[i];
+            } */
+            
+        }
+
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         OnLand = true;
+        //Seeds.SetActive(false);
         Debug.Log("In the soil");
-        Seeds.SetActive(false);
+        
         //Destroy(newInstance);
+        //As time passes, the plants grow
+        
     }
 }
